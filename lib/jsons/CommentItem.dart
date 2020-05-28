fae1a21164db576f08fcecf3746b13b9a5d5e1d6
DarkiHJS <125730892@qq.com>
2020-05-28 18:11:58 +0800

    content page

---
+++ lib/jsons/CommentItem.dart
@@ -0,0 +1,166 @@
+class CommentItem {
+  String id;
+  String content;
+  // Null createTime;
+  String memberId;
+  String praiseNum;
+  String rescueId;
+  // Null startId;
+  // Null toMemberId;
+  // Null toMemberNickname;
+  MemberDetail memberDetail;
+  int childrenCount;
+  List<Childrens> childrens;
+
+  CommentItem(
+      {this.id,
+      this.content,
+      // this.createTime,
+      this.memberId,
+      this.praiseNum,
+      this.rescueId,
+      // this.startId,
+      // this.toMemberId,
+      // this.toMemberNickname,
+      this.memberDetail,
+      this.childrenCount,
+      this.childrens});
+
+  CommentItem.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    content = json['content'];
+    // createTime = json['create_time'];
+    memberId = json['member_id'];
+    praiseNum = json['praise_num'];
+    rescueId = json['rescue_id'];
+    // startId = json['start_id'];
+    // toMemberId = json['to_member_id'];
+    // toMemberNickname = json['to_member_nickname'];
+    memberDetail = json['member_detail'] != null
+        ? new MemberDetail.fromJson(json['member_detail'])
+        : null;
+    childrenCount = json['children_count'];
+    if (json['childrens'] != null) {
+      childrens = new List<Childrens>();
+      json['childrens'].forEach((v) {
+        childrens.add(new Childrens.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['content'] = this.content;
+    // data['create_time'] = this.createTime;
+    data['member_id'] = this.memberId;
+    data['praise_num'] = this.praiseNum;
+    data['rescue_id'] = this.rescueId;
+    // data['start_id'] = this.startId;
+    // data['to_member_id'] = this.toMemberId;
+    // data['to_member_nickname'] = this.toMemberNickname;
+    if (this.memberDetail != null) {
+      data['member_detail'] = this.memberDetail.toJson();
+    }
+    data['children_count'] = this.childrenCount;
+    if (this.childrens != null) {
+      data['childrens'] = this.childrens.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class MemberDetail {
+  String id;
+  String nickname;
+  String avatar;
+
+  MemberDetail({this.id, this.nickname, this.avatar});
+
+  MemberDetail.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    nickname = json['nickname'];
+    avatar = json['avatar'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['nickname'] = this.nickname;
+    data['avatar'] = this.avatar;
+    return data;
+  }
+}
+
+class Childrens {
+  String id;
+  String startId;
+  String pid;
+  String content;
+  String rescueId;
+  String level;
+  String memberId;
+  String toMemberId;
+  String toMemberNickname;
+  String praiseNum;
+  String status;
+  Null createTime;
+  Null updateTime;
+  MemberDetail memberDetail;
+
+  Childrens(
+      {this.id,
+      this.startId,
+      this.pid,
+      this.content,
+      this.rescueId,
+      this.level,
+      this.memberId,
+      this.toMemberId,
+      this.toMemberNickname,
+      this.praiseNum,
+      this.status,
+      this.createTime,
+      this.updateTime,
+      this.memberDetail});
+
+  Childrens.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    startId = json['start_id'];
+    pid = json['pid'];
+    content = json['content'];
+    rescueId = json['rescue_id'];
+    level = json['level'];
+    memberId = json['member_id'];
+    toMemberId = json['to_member_id'];
+    toMemberNickname = json['to_member_nickname'];
+    praiseNum = json['praise_num'];
+    status = json['status'];
+    createTime = json['create_time'];
+    updateTime = json['update_time'];
+    memberDetail = json['member_detail'] != null
+        ? new MemberDetail.fromJson(json['member_detail'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['start_id'] = this.startId;
+    data['pid'] = this.pid;
+    data['content'] = this.content;
+    data['rescue_id'] = this.rescueId;
+    data['level'] = this.level;
+    data['member_id'] = this.memberId;
+    data['to_member_id'] = this.toMemberId;
+    data['to_member_nickname'] = this.toMemberNickname;
+    data['praise_num'] = this.praiseNum;
+    data['status'] = this.status;
+    data['create_time'] = this.createTime;
+    data['update_time'] = this.updateTime;
+    if (this.memberDetail != null) {
+      data['member_detail'] = this.memberDetail.toJson();
+    }
+    return data;
+  }
+}
--- lib/pages/home/DetailsArticle.dart
+++ lib/pages/home/DetailsArticle.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:hello_world/jsons/CommentItem.dart';
 import 'package:hello_world/jsons/ContentDetailsData.dart';
 import 'package:hello_world/utils/Request.dart';
 
@@ -23,20 +24,20 @@ class _DetailsArticlePageState extends State<DetailsArticlePage> {
   Extends _maxSwiper;
   _DetailsArticlePageState(this.id);
 
-  Future<ContentDetailsData> getPageData() async {
+  Future getPageData() async {
     Map<String, dynamic> data = await PxzRequest().get("/rescue/detail/1");
     setState(() {
       double maxHight = 0;
       _contentDetailsData = ContentDetailsData.fromJson(data["data"]);
       _contentDetailsData.resources.forEach((element) {
-        if(maxHight < double.parse(element.whextends.height)) {
+        if (maxHight < double.parse(element.whextends.height)) {
           maxHight = double.parse(element.whextends.height);
           _maxSwiper = element.whextends;
         }
       });
       print(_maxSwiper.width);
     });
-   
+
     // return null;
   }
 
@@ -44,135 +45,290 @@ class _DetailsArticlePageState extends State<DetailsArticlePage> {
   void initState() {
     super.initState();
     getPageData();
-    
   }
 
   @override
   Widget build(BuildContext context) {
-    print(double.parse(_maxSwiper.height) / (double.parse(_maxSwiper.width) / MediaQuery.of(context).size.width));
     return Scaffold(
-      appBar: PreferredSize(
-          child: AppBar(
-            brightness: Brightness.light,
-            backgroundColor: Colors.transparent,
-            elevation: 0.0,
-          ),
-          preferredSize: Size.fromHeight(0)),
-      body: _contentDetailsData == null ? SizedBox.shrink() : CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            automaticallyImplyLeading: false,
-            backgroundColor: Colors.white,
-            title: Row(
-              children: <Widget>[
-                IconButton(
-                  color: Colors.grey,
-                  alignment: Alignment.centerLeft,
-                  icon: Icon(Icons.arrow_back_ios),
-                  onPressed: () => Navigator.of(context).pop(),
-                  padding: EdgeInsets.zero,
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 0, 8, 0),
-                  child: CircleAvatar(
-                    radius: 15,
-                    backgroundImage: NetworkImage(
-                        "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1906469856,4113625838&fm=26&gp=0.jpg"),
-                  ),
-                ),
-                Text(
-                  "女王草莓",
-                  style: TextStyle(fontSize: 14),
-                ),
-                Expanded(child: SizedBox.shrink()),
-                SizedBox(
-                  width: 50,
-                  height: 22,
-                  child: OutlineButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25)),
-                    borderSide: BorderSide(color: Colors.yellow),
-                    padding: EdgeInsets.zero,
-                    child: Text(
-                      "关注",
-                      style: TextStyle(fontSize: 12, color: Colors.yellow),
-                    ),
-                    onPressed: () {},
-                  ),
-                ),
-                IconButton(
-                  alignment: Alignment.centerRight,
-                  padding: EdgeInsets.all(0),
-                  color: Colors.grey,
-                  icon: Icon(Icons.open_in_new),
-                  onPressed: () {},
-                )
-              ],
+        appBar: PreferredSize(
+            child: AppBar(
+              brightness: Brightness.light,
+              backgroundColor: Colors.transparent,
+              elevation: 0.0,
             ),
+            preferredSize: Size.fromHeight(0)),
+        body: _contentDetailsData == null
+            ? SizedBox.shrink()
+            : Stack(
+                children: <Widget>[
+                  CustomScrollView(
+                    slivers: <Widget>[
+                      SliverAppBar(
+                        automaticallyImplyLeading: false,
+                        pinned: true,
+                        backgroundColor: Colors.white,
+                        elevation: 0.0,
+                        title: Row(
+                          children: <Widget>[
+                            IconButton(
+                              color: Colors.grey,
+                              alignment: Alignment.centerLeft,
+                              icon: Icon(Icons.arrow_back_ios),
+                              onPressed: () => Navigator.of(context).pop(),
+                              padding: EdgeInsets.zero,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(0, 0, 8, 0),
+                              child: CircleAvatar(
+                                radius: 15,
+                                backgroundImage: NetworkImage(
+                                    "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1906469856,4113625838&fm=26&gp=0.jpg"),
+                              ),
+                            ),
+                            Text(
+                              "女王草莓",
+                              style: TextStyle(fontSize: 14),
+                            ),
+                            Expanded(child: SizedBox.shrink()),
+                            SizedBox(
+                              width: 50,
+                              height: 22,
+                              child: OutlineButton(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(25)),
+                                borderSide: BorderSide(color: Colors.yellow),
+                                padding: EdgeInsets.zero,
+                                child: Text(
+                                  "关注",
+                                  style: TextStyle(
+                                      fontSize: 12, color: Colors.yellow),
+                                ),
+                                onPressed: () {},
+                              ),
+                            ),
+                            IconButton(
+                              alignment: Alignment.centerRight,
+                              padding: EdgeInsets.all(0),
+                              color: Colors.grey,
+                              icon: Icon(Icons.open_in_new),
+                              onPressed: () {},
+                            )
+                          ],
+                        ),
+                      ),
+                      SliverList(
+                        delegate: SliverChildListDelegate(<Widget>[
+                          Container(
+                              height: double.parse(_maxSwiper.height) /
+                                      (double.parse(_maxSwiper.width) /
+                                          MediaQuery.of(context).size.width) +
+                                  30,
+                              alignment: Alignment.topCenter,
+                              child: Swiper(
+                                  itemCount:
+                                      _contentDetailsData.resources.length,
+                                  itemBuilder:
+                                      (BuildContext context, int index) {
+                                    return Column(
+                                      children: <Widget>[
+                                        CachedNetworkImage(
+                                          height: double.parse(
+                                                  _maxSwiper.height) /
+                                              (double.parse(_maxSwiper.width) /
+                                                  MediaQuery.of(context)
+                                                      .size
+                                                      .width),
+                                          alignment: Alignment.center,
+                                          imageUrl: _contentDetailsData
+                                              .resources[index].url,
+                                          fit: BoxFit.fitWidth,
+                                        ),
+                                        SizedBox.shrink()
+                                      ],
+                                    );
+                                  },
+                                  // control: new SwiperControl(),
+                                  pagination: SwiperPagination(
+                                      builder: DotSwiperPaginationBuilder(
+                                    color: Color(0xffdfdfdf),
+                                    activeColor: Colors.yellow,
+                                    size: 5,
+                                    activeSize: 7,
+                                  )))),
+                          Article(
+                            title: _contentDetailsData.title,
+                            desc: _contentDetailsData.desc,
+                          ),
+                          Padding(padding: EdgeInsets.symmetric(horizontal: 15), child: Divider(height: 20,color: Colors.grey,),),
+                          CommentList(id: "1",),
+                          SizedBox.fromSize(
+                            size: Size.fromHeight(55),
+                          )
+                        ]),
+                      ),
+                    ],
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: DetailsBottomBar()
+                  ),
+                  
+                ],
+              ));
+  }
+}
+
+// 文章组件
+class Article extends StatelessWidget {
+  final String title;
+  final String desc;
+  const Article({Key key, this.title, this.desc}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            title,
+            textAlign: TextAlign.left,
+            style: TextStyle(fontSize: 18),
           ),
-          SliverList(
-            delegate: SliverChildListDelegate(<Widget>[
-              Container(
-                height: double.parse(_maxSwiper.height) / (double.parse(_maxSwiper.width) / MediaQuery.of(context).size.width),
-                child: Swiper(
-                  itemCount: _contentDetailsData.resources.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return CachedNetworkImage(
-                        imageUrl: _contentDetailsData.resources[index].url,
-                        fit: BoxFit.fitWidth,
-                      );
-                  },
-                  // control: new SwiperControl(),
-                ),
-              )
-            ]),
-          )
+          Text(desc),
         ],
       ),
-      // Container(
-      //   width: double.infinity,
-      //   height: double.infinity,
-      //   decoration: BoxDecoration(color: Colors.white),
-      //   child: FutureBuilder(
-      //     future: getPageData(),
-      //     builder: (BuildContext context, AsyncSnapshot snapshot) {
-      //       if (snapshot.connectionState == ConnectionState.done) {
-      //         ContentDetailsData data = snapshot.data as ContentDetailsData;
-      //         return Column(
-      //           children: <Widget>[
-      //              Container(
-      //               height: double.infinity,
-      //               child: ListView(
-      //                 children: <Widget>[
-      //                   Swiper(
-      //                     itemCount: 2,
-      //                     itemBuilder: (BuildContext context, int index) {
-      //                       return [
-      //                         Image.network(
-      //                           "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2364654808,1367649755&fm=26&gp=0.jpg",
-      //                           fit: BoxFit.fitWidth,
-      //                         ),
-      //                         Image.network(
-      //                           "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1589795536301&di=e65c12f32f0969f9337c309db95209cb&imgtype=0&src=http%3A%2F%2Fwww.chabeichong.com%2Fimages%2F2016%2F10%2F21-06155760.jpg",
-      //                           fit: BoxFit.fitWidth,
-      //                         )
-      //                       ][index];
-      //                     },
-      //                     control: new SwiperControl(),
-      //                   )
-      //                 ],
-      //               ),
-      //             )
-      //           ],
-      //         );
-      //       } else {
-      //         return Center(
-      //           child: Image.asset("assets/details/details_loading.gif"),
-      //         );
-      //       }
-      //     },
-      //   ),
-      // ),
+    );
+  }
+}
+
+// bottom bar
+class DetailsBottomBar extends StatefulWidget {
+  @override
+  _DetailsBottomBarState createState() => _DetailsBottomBarState();
+}
+
+class _DetailsBottomBarState extends State<DetailsBottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 5),
+      height: 45,
+      decoration: BoxDecoration(
+        border: Border(top: BorderSide(color:  Theme.of(context).dividerColor)),
+        color: Colors.white
+      ),
+      child: Row(
+        children: <Widget>[
+          FlatButton.icon(
+            onPressed: null, 
+            icon: Icon(Icons.create), 
+            label: Text("说点什么吧……")),
+          Expanded(child: SizedBox.shrink()),
+          MaterialButton(
+            onPressed: null,
+            minWidth: 0,
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.favorite_border),
+                Text("1151"),
+              ], 
+            ),
+          ),
+          MaterialButton(
+            onPressed: null,
+            minWidth: 0,
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.star_border),
+                Text("1151"),
+              ], 
+            ),
+          ),
+          MaterialButton(
+            onPressed: null,
+            minWidth: 0,
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.chat_bubble_outline),
+                Text("1151"),
+              ], 
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/// 评论列表
+/// 
+
+class CommentList extends StatefulWidget {
+  final String id;
+  CommentList({Key key, this.id}) : super(key: key);
+
+  @override
+  _CommentListState createState() => _CommentListState(id);
+}
+
+class _CommentListState extends State<CommentList> {
+  String id;
+  _CommentListState(this.id);
+  
+  List<CommentItem> _commentItems = [];
+  int _totalItems;
+  int _index = 1;
+  Future getCommentList() async {
+    Map<String, dynamic> data = 
+      await PxzRequest().get(
+        "/rescue/get_comment", 
+        data: {
+          "id": id,
+          "page": _index,
+          "limit": 10
+        }
+      );
+    setState(() {
+      data["data"]["items"].forEach((element) {
+        _commentItems.add(CommentItem.fromJson(element));
+      });
+      _totalItems = data["data"]["total_items"];
+    });
+    print("请求数据");
+    print(_commentItems);
+    print(_totalItems);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCommentList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: _commentItems ?? SliverList(
+        delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+          return Row(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 15,
+                backgroundImage: NetworkImage(_commentItems[index].memberDetail.avatar),
+              )
+            ],  
+          );
+        },
+        childCount: _commentItems.length
+      )
+    )
     );
   }
 }
